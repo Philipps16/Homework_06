ac5f93d1c52c29bf3c6029ff09dc517d1e7edf94
Philipp <press.fm@mail.ru>
2022-12-21 22:08:41 +0300

    Выполнил упражнение 01

---
+++ Example_01_sum_+/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите числа: ");
+int [] numbers = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int count = 0;
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > 0)
+    count++;
+}
+Console.WriteLine($"Количество введенных положительных чисел: {count}");
