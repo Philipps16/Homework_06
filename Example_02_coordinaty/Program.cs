2d78ee908df7ee488669a42a8c4b4d5e8f2fb527
Philipp <press.fm@mail.ru>
2022-12-21 22:28:52 +0300

    Начал Упражнение 02

---
+++ Example_02_coordinaty/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число b1");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите число k1");
+double k1 = Convert.ToDouble(Console.ReadLine());
+
+
+Console.WriteLine("Введите число b2");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите число k2");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+double y1 = k1 * x + b1;
+double y2 = k2 * x + b2;
+
+double x = ()
