336201836eaa487070141ba9c09fd2e6d9efd7d2
Philipp Mitrofanov <press.fm@mail.ru>
2022-12-22 10:16:43 +0300

    Выполнил упражнение02

--- Example_02_coordinaty/Program.cs
+++ Example_02_coordinaty/Program.cs
@@ -3,14 +3,12 @@ Console.WriteLine("Введите число b1");
 double b1 = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("Введите число k1");
 double k1 = Convert.ToDouble(Console.ReadLine());
-
-
 Console.WriteLine("Введите число b2");
 double b2 = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("Введите число k2");
 double k2 = Convert.ToDouble(Console.ReadLine());
 
-double y1 = k1 * x + b1;
-double y2 = k2 * x + b2;
+double x = - (b1 - b2)/(k1 - k2);
+double y = k1 * x + b1;
 
-double x = ()
+Console.WriteLine($"Координаты точки пересечения равны [{Math.Round(x, 1)}, {Math.Round(y, 1)}]");
